bd6b7fc8983bbd84d7ae7b671ef4f2848bcf3b67
Diana Laxmytkina <dlaxmytkina@mail.ru>
2021-09-20 21:25:58 +0300

    kkn

--- script.js
+++ script.js
@@ -1,73 +1,88 @@
-$(function(){
+$(function () {
+    let scrollBottom = $(window).scrollTop() + $(window).height() - 1;
+    let scrollTop = $(window).scrollTop();
+scrollAnimate(scrollBottom, scrollTop);
 
 
+    $('a').click(function (e) {
 
-        
-        $('a').click (function(e) {
-            
-          e.preventDefault();
-          var id  = $(this).attr('href'),
-              top = $(id).offset().top;
-          $('html, body').animate({scrollTop: top}, 1500);
+        e.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('html, body').animate({ scrollTop: top }, 1500);
+
+    });
+
+    function pos(elem) {
+        var elem_top = $(elem).offset().top,
+            elem_bottom = elem_top + $(elem).outerHeight();
+        return {
+            top: elem_top,
+            bottom: elem_bottom
+        }
+    }
+
+    $(window).scroll(function(){
+        scrollBottom = $(window).scrollTop() + $(window).height() - 1;
+         scrollTop = $(window).scrollTop();
+         scrollAnimate(scrollBottom, scrollTop);
+    })
+
+    function scrollAnimate(scrollBottom, scrollTop){
          
-         });
 
-         function pos (elem){
-             var elem_top = $(elem).offset().top,
-              elem_bottom = elem_top + $(elem).outerHeight();
-             return{
-                 top: elem_top,
-                 bottom: elem_bottom
-             }
-         }
-         
-         $(window).scroll(function(e){
+        if (pos(".block_aboutUs").top < scrollBottom && pos($(".block_aboutUs ")).bottom > scrollTop) {
 
-            let scrollBottom = $(window).scrollTop()+$(window).height()-1;
-            let scrollTop = $(window).scrollTop();
+            $('.block_aboutUs .flex_block').css({
+                'animation': 'slideUp 1.5s',
+                'opacity': '1'
+            });
 
-            if(pos(".block_aboutUs").top < scrollBottom && pos($(".block_aboutUs")).bottom >scrollTop) {
-  
-                $('.block_aboutUs .flex_block').css({'animation':'slideUp 1.5s',
-            'display': 'flex'});
-             
-               } 
-                if(pos(".block_team .flex_block").top < scrollBottom && pos(".block_team .flex_block").bottom >scrollTop) {
-                
-                   $('.block_team .flex_block').css('animation','slideUp 1.5s');
-                
-                  } 
+        }
+        if (pos(".block_team ").top < scrollBottom && pos(".block_team ").bottom > scrollTop) {
 
-                  if(pos(".booking .flex_block").top < scrollBottom && pos(".booking .flex_block").bottom >scrollTop) {
-                
-                   $('.booking_form_block').css('animation','scale 1.5s');
-                
-                  }  
-               
-                  if(pos(".block_specialties .flex_block").top < scrollBottom && pos(".block_specialties .flex_block").bottom >scrollTop) {
-                
-                   $('.block_specialties .flex_block').css('animation','slideUp 1.5s');
-                
-                  }   
-                  if(pos(".block_menu .menu").top < scrollBottom && pos(".block_menu .menu").bottom >scrollTop) {
-                
-                   $('.block_menu .menu').css('animation','slideUp 1.5s');
-                
-                  } 
-                  if(pos(".block_privateEvents .flex_block").top < scrollBottom && pos(".block_privateEvents .flex_block").bottom >scrollTop) {
-                
-                   $('.block_privateEvents .flex_block').css('animation','slideUp 1.5s');
-                
-                  } 
-                  else if (($(window).height() )> scrollBottom){
-                   $('.block_aboutUs .flex_block, .block_privateEvents .flex_block, .block_menu .menu, .block_specialties .flex_block, .booking_form_block, .block_team .flex_block ').css('animation','none');
-                  
-               }
-         }) 
+            $('.block_team .flex_block').css({
+                'animation': 'slideUp 1.5s',
+                'opacity': '1'
+            });
 
+        }
+
+        if (pos(".booking ").top < scrollBottom && pos(".booking ").bottom > scrollTop) {
+
+            $('.booking_form_block').css({
+                'animation': 'scale 1.5s',
+                'opacity': '1'
+            });
+
+        }
+
+        if (pos(".block_specialties ").top < scrollBottom && pos(".block_specialties ").bottom > scrollTop) {
+
+            $('.block_specialties .flex_block').css({
+                'animation': 'slideUp 1.5s',
+                'opacity': '1'
+            });
+
+        }
+        if (pos(".block_menu ").top < scrollBottom && pos(".block_menu ").bottom > scrollTop) {
+
+            $('.block_menu .menu').css({'animation':'slideUp 1.5s', 'opacity':'1'});
+
+        }
+        if (pos(".block_privateEvents ").top < scrollBottom && pos(".block_privateEvents ").bottom > scrollTop) {
+
+            $('.block_privateEvents .flex_block').css({
+                'animation': 'slideUp 1.5s',
+                'opacity': '1'
+            });
+
+        }
+      
+    }
 
     var booking_input = $('input');
-    var menu_position= `<div class="position">
+    var menu_position = `<div class="position">
                         <div>
                             <h4>PIZZA QUATRO STAGIONI . . .  &nbsp</h4>
                             <p>Integer ullamcorper neque eu purus euismod</p>
@@ -75,47 +90,48 @@ $(function(){
                         <h4>55,68 USD</h4>
                     </div>`;
 
-(function () {
-    for(i=0;i<21;i++){
-        $('.menu_positions').append(menu_position);
-        $('.menu_positions').append('');
-        console.log(i)
-    }
- }());
- let today = new Date();
+    (function () {
+        for (i = 0; i < 21; i++) {
+            $('.menu_positions').append(menu_position);
+            $('.menu_positions').append('');
+            console.log(i)
+        }
+    }());
+    let today = new Date();
     hour = today.getHours(),
-    min = today.getMinutes(),
-    day = today.getDate(),
-    month = today.getMonth()+1;
+        min = today.getMinutes(),
+        day = today.getDate(),
+        month = today.getMonth() + 1;
     year = today.getFullYear()
     console.log(year);
- 
-    if(today.getDate()<10){
-         day = `0${day}`;
-    } 
-    if(month<10){
-month = `0${month}`;
+
+    if (today.getDate() < 10) {
+        day = `0${day}`;
+    }
+    if (month < 10) {
+        month = `0${month}`;
     }
 
-    booking_input.each(function(){
-        if ($(this).attr('placeholder')==='Date (mm/dd)'){
-             console.log('ggg');
-            $(this).attr( 'min',`${year}-${month}-${day}`);
-            if((today.getMonth()+3)<10){
-                month = `0${today.getMonth()+3}`;
-                    } else month = today.getMonth()+3;
-               $(this).attr('max',`${year}-${ month}-${day}`)
-            }})
-
-      
-
-    booking_input.focus(function() {
-    booking_input.each(function(){
-        if($(this).attr('placeholder')==='Phone'){
-            $(this).mask("+375(99) 999-99-99");
+    booking_input.each(function () {
+        if ($(this).attr('placeholder') === 'Date (mm/dd)') {
+            console.log('ggg');
+            $(this).attr('min', `${year}-${month}-${day}`);
+            if ((today.getMonth() + 3) < 10) {
+                month = `0${today.getMonth() + 3}`;
+            } else month = today.getMonth() + 3;
+            $(this).attr('max', `${year}-${month}-${day}`)
         }
     })
-}
+
+
+
+    booking_input.focus(function () {
+        booking_input.each(function () {
+            if ($(this).attr('placeholder') === 'Phone') {
+                $(this).mask("+375(99) 999-99-99");
+            }
+        })
+    }
     )
 
 })
